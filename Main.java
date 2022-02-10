0c49a12b083eff3978d56721f2267450afaa1ef6
Cuauh Medina <cuauh_medina@hotmail.com>
2022-02-10 15:46:32 -0600

    TEMO-002:
    Adding line to include the number 5

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String args []){
         System.out.println("This is a Hello world in second commit, branch TEMO-002");
+        System.out.println("Now we print the number five");
+        printNumber(5);
     }
 
     public static void printNumber(int numb){
-        System.out.println("this is the numner " + numb);
+        System.out.println("this is the number " + numb);
     }
 }
