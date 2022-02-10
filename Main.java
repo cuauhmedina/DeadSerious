8079c017dccdd9c9caa2a67a3724d12358f20d8c
Cuauh Medina <cuauh_medina@hotmail.com>
2022-02-10 15:52:39 -0600

    TEMO-003
    This creates a new method to print a letter

--- Main.java
+++ Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String args []){
         System.out.println("This is a Hello world in first commit");
     }
+
+    public static void printLetter(String letter){
+        System.out.println("This is a the letter " + letter);
+    }
 }
