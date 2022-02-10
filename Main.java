c7267c11985df9326936feb0a00bc5a366f802fe
Cuauh Medina <cuauh_medina@hotmail.com>
2022-02-10 15:39:51 -0600

    TEMO-002
    Adding new branch for 002 and new method to print numb

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String args []){
-        System.out.println("This is a Hello world in first commit");
+        System.out.println("This is a Hello world in second commit, branch TEMO-002");
+    }
+
+    public static void printNumber(int numb){
+        System.out.println("this is the numner " + numb);
     }
 }
